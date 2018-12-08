f5f85a60fb5464991882c53c15566b0785a6d5ac
gihooh <carloginocatapang@gmail.com>
2018-12-08 16:40:04 +0800

    Integrated webpack scripts

---
+++ src/client/components/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = () => {
+  return <div>I'm the Home component</div>
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 
+const React = require('react');
+const renderToString = require('react-dom/server').renderToString;
+const Home = require('./client/components/Home').default;
+
 const app = express();
+
+app.get('/', (req, res) => {
+  const content = renderToString(<Home />);
+
+  res.send(content);
+});
 
 app.listen(3000, () => {
   console.log(`Listening to port 3000`);
